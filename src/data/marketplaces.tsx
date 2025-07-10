48c3dcabf0c87bd40f9d8464dea82a01c28d5ea2
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2025-07-10 13:54:31 +0700

    fix: ayoomall conf

--- src/data/marketplaces.tsx
+++ src/data/marketplaces.tsx
@@ -25,14 +25,15 @@ export const marketplaces: Marketplace[] = [
   },
   {
     meta: {
-      auth: "https://api.dev.ayoomall.com/v1.0/auth/lkpp/login",
+      auth: "https://dev.ayoomall.com/lkpp/login",
       clientId: "FRFKM6GNI5R3V",
       clientSecret: "MYDLI0R9SIGGRBS8A7EWPJZ6WFBXT",
     },
     icon: "https://media-bela-rajastore.airmasgroup.co.id/logo/sVAxed8qnoAPcyilvvhV8UTcYdGUd3kLJMdoBK9t.png",
     slug: "ayoomall-dev",
     name: "AyooMall",
-    link: (cat: string, token?: string) => `https://dev.ayoomall.com/`,
+    link: (cat: string, token?: string) =>
+      `https://dev.ayoomall.com/category/${cat}?nonce=${token}`,
   },
 ];
 
